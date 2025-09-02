168e7c40a569382729abfc0a39cab8366aa8333d
Dev Suthar <4ev3rdk@gmail.com>
2025-09-02 23:20:49 +0530

    Added Marketing Page

---
+++ planzo-next.js/app/(marketing)/layout.tsx
@@ -0,0 +1,10 @@
+const MarketingPage = ({ children }: { children: React.ReactNode }) => {
+
+    return (<div className="h-full bg-slate-600">
+
+        <main className="pt-40 pb-20 bg-slate-100"  >
+            {children}
+        </main>
+    </div >)
+};
+export default MarketingPage; 
